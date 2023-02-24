279f2f8c04b2bf4b9ede98565d8c2e176bf64c03
vasu <vasuparsaniya21@gmail.com>
2023-02-25 00:43:14 +0530

    Hello4

--- Login/signup.php
+++ Login/signup.php
@@ -16,7 +16,6 @@
 			<input type="password" name="password" placeholder="Enter Password">
 			<input type="submit" name="submit" value="Sign Up">
 			<p>Already have an account? <a href="login.php">Login</a></p>
-			
 		</form>
 	</div>
 </body>
